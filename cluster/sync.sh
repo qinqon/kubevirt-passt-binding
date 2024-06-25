1bf3cd4b4468a4993be3fe6b13b88213ed795690
Enrique Llorente <ellorent@redhat.com>
2024-06-25 12:35:55 +0200

    Make cluster-sync
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

---
+++ cluster/sync.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -xe
+sidecar_image=quay.io/ellorent/kubevirt-passt-binding
+sidecar_image_sha=$(skopeo inspect docker://$sidecar_image | jq -r .Digest)
+
+kubectl patch kubevirts -n kubevirt kubevirt --type=json -p="[{\"op\": \"add\", \"path\": \"/spec/configuration/network\",   \"value\": {
+      \"binding\": {
+          \"passt\": {
+              \"networkAttachmentDefinition\": \"netbindingpasst\",
+              \"sidecarImage\": \"${sidecar_image}@${sidecar_image_sha}\",
+              \"migration\": {
+                  \"method\": \"link-refresh\"
+              }
+          }
+      }
+  }}]"
+
+for node in $(kubectl get node --no-headers  -o custom-columns=":metadata.name"); do
+    docker cp ./kubevirt-passt-binding $node:/opt/cni/bin/kubevirt-passt-binding
+done
